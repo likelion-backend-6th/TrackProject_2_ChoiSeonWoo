0f2dea1739da4c20accf99d5c45156532e071f44
25th-Night <brown_eyed87@naver.com>
2023-09-14 03:04:21 +0900

    users/urls: 생성한 View를 url에 등록

--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,23 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from users.views import (
-    FollowViewSet,
-    ProfileViewSet,
+    TokenObtainPairView_,
+    TokenRefreshView_,
     SignUpView,
     LoginView,
     UserViewSet,
-    TokenObtainPairView_,
-    TokenRefreshView_,
+    ProfileViewSet,
+    UserInfoView,
+    MyInfoView,
+    ProfileView,
+    FollowView,
+    FollowingView,
+    FollowerView,
+    FeedView,
 )
 
 router = DefaultRouter()
 router.register("profiles", ProfileViewSet, basename="profile")
-router.register("follows", FollowViewSet, basename="follow")
 router.register("", UserViewSet, basename="user")
 # router.register("path명", ViewSet클래스, basename="basename지정")
 
@@ -21,5 +26,12 @@ urlpatterns = [
     path("token/refresh/", TokenRefreshView_.as_view(), name="token_refresh"),
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
+    path("others/", UserInfoView.as_view(), name="other_users"),
+    path("myinfo/", MyInfoView.as_view(), name="myinfo"),
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path("follow/<int:id>/", FollowView.as_view(), name="follow"),
+    path("following/", FollowingView.as_view(), name="following"),
+    path("follower/", FollowerView.as_view(), name="follower"),
+    path("feed/", FeedView.as_view(), name="feed"),
     path("", include(router.urls)),
 ]
