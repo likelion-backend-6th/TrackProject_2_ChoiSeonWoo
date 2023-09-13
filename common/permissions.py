add360970b67c0bded0bcb53c1af0e4c6ca01f74
25th-Night <brown_eyed87@naver.com>
2023-09-14 02:29:35 +0900

    common/permissions: CustomReadOnly 클래스 재추가

--- common/permissions.py
+++ common/permissions.py
@@ -39,3 +39,42 @@ class IsAdminOrReadOnly(BasePermission):
         elif user.is_admin:
             return True
         return False
+
+
+class CustomReadOnly(BasePermission):
+    SAFE_METHODS = ("GET",)
+    attributes_to_check = ("email", "user", "user_from", "author")
+    message = "Not Allowed"
+
+    @staticmethod
+    def class_name(obj):
+        return type(obj).__name__
+
+    def has_permission(self, request, view):
+        user = request.user
+        if request.method in self.SAFE_METHODS:
+            return True
+        elif user.is_authenticated:
+            return True
+        elif not user.is_authenticated:
+            response = {
+                "detail": "Please login to use the service.",
+            }
+            raise GenericAPIException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail=response
+            )
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+
+            return True
+        if self.class_name(obj) == "User" and obj.email == user.email:
+            return True
+        for attr in self.attributes_to_check:
+            if hasattr(obj, attr):
+                attr_value = getattr(obj, attr)
+                if user == attr_value:
+                    return True
+                return False
+        return False
