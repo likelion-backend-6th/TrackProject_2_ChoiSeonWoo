4ac3994143159b5182faea10facc0fc7a7fecc6d
25th-Night <brown_eyed87@naver.com>
2023-09-16 23:37:49 +0900

    posts/views: CommentViewSet, MyCommentListView 추가

--- posts/views.py
+++ posts/views.py
@@ -9,9 +9,9 @@ from drf_spectacular.utils import extend_schema
 
 from django_filters.rest_framework import DjangoFilterBackend
 
-from posts.models import Post
-from posts.serializers import PostSerializer
-from posts.filters import PostFilter
+from posts.models import Comment, Post
+from posts.serializers import CommentSerializer, PostSerializer
+from posts.filters import PostFilter, CommentFilter
 from posts.permissions import CommonUserPermission
 from users.models import User
 
@@ -52,6 +52,53 @@ class MyPostListView(APIView):
     def get(self, request):
         user: User = request.user
         posts = user.posts.all()
+        queryset = self.filter_backends().filter_queryset(request, posts, self)
+
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@extend_schema(tags=["06. Comment"])
+class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = [CommonUserPermission]
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    filterset_class = CommentFilter
+
+    def get_queryset(self):
+        post_pk = self.kwargs["post_pk"]
+        queryset = Comment.objects.filter(post__pk=post_pk)
+        comment_pk = self.kwargs.get("id")
+        if comment_pk:
+            queryset = queryset.filter(id=comment_pk)
+
+        return queryset
+
+    def list(self, request: Request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        post_pk = kwargs.get("post_pk")
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data["post_id"] = post_pk
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@extend_schema(tags=["01. My - Comment"])
+class MyCommentListView(APIView):
+    serializer_class = CommentSerializer
+    filter_backends = DjangoFilterBackend
+    filterset_class = CommentFilter
+
+    def get(self, request):
+        user: User = request.user
+        posts = user.comments.all()
         queryset = self.filter_backends().filter_queryset(request, posts, self)
 
         serializer = self.serializer_class(queryset, many=True)
