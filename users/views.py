10e5dbf3031e50bbb4a3d6c21c0eedcb29d1867f
25th-Night <brown_eyed87@naver.com>
2023-09-18 01:35:22 +0900

    users/views: Profile, Follow 로직 수정에 따른 View 추가/제거

--- users/views.py
+++ users/views.py
@@ -55,7 +55,7 @@ class SignUpView(APIView):
             return Response(
                 {
                     "user": serializer.data,
-                    "message": "register successs",
+                    "detail": "register successs",
                     "token": {"access": access_token, "refresh": refresh_token},
                 },
                 status=status.HTTP_201_CREATED,
@@ -82,14 +82,12 @@ class LoginView(APIView):
             return Response(
                 {
                     "user": serializer.data,
-                    "message": "login successs",
+                    "detail": "login successs",
                     "token": {"access": access_token, "refresh": refresh_token},
                 },
                 status=status.HTTP_200_OK,
             )
-        return Response(
-            {"message": "login failed"}, status=status.HTTP_401_UNAUTHORIZED
-        )
+        return Response({"detail": "login failed"}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 @extend_schema(tags=["00. Auth"])
@@ -117,42 +115,37 @@ class UserViewSet(viewsets.ModelViewSet):
     @extend_schema(deprecated=True)
     def create(self, request: Request, *args, **kwargs):
         return Response(
-            data={"message": "Create operation is not supported"},
+            data={"detail": "Create operation is not supported"},
             status=status.HTTP_404_NOT_FOUND,
         )
 
-    @action(detail=True, methods=["get"], url_name="profile")
-    def profile(self, request: Request, *args, **kwargs):
-        user: User = self.get_object()
-
-        profile = user.profile
-        serializer = ProfileSerializer(profile)
-
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
-
+    @extend_schema(tags=["05. Follow"])
     @action(
         detail=True,
         methods=["post"],
         url_path=r"follow/(?P<id>[0-9]+)",
     )
     def follow(self, request: Request, pk, id):
-        user: User = User.objects.get(id=pk)
+        follower: User = get_object_or_404(User, id=pk)
+        followee: User = get_object_or_404(User, id=id)
 
-        followee = get_object_or_404(User, id=id)
-        follow, created = Follow.objects.get_or_create(user_from=user, user_to=followee)
+        follow, created = Follow.objects.get_or_create(
+            user_from=follower, user_to=followee
+        )
 
         if created:
             return Response(
-                data={"message": "Follow successfully"}, status=status.HTTP_201_CREATED
+                data={"detail": "Follow successfully"}, status=status.HTTP_201_CREATED
             )
 
         follow.delete()
 
         return Response(
-            data={"message": "Unfollowed successfully"},
+            data={"detail": "Unfollowed successfully"},
             status=status.HTTP_204_NO_CONTENT,
         )
 
+    @extend_schema(tags=["05. Follow"])
     @action(detail=True, methods=["get"], url_name="following")
     def following(self, request: Request, *args, **kwargs):
         user: User = self.get_object()
@@ -161,6 +154,7 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(following, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
+    @extend_schema(tags=["05. Follow"])
     @action(detail=True, methods=["get"], url_name="follower")
     def follower(self, request: Request, *args, **kwargs):
         user: User = self.get_object()
@@ -169,6 +163,7 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(following, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
+    @extend_schema(tags=["06. Post"])
     @action(detail=True, methods=["get"], url_name="posts")
     def posts(self, request: Request, *args, **kwargs):
         user: User = self.get_object()
@@ -177,6 +172,7 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = PostSerializer(posts, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
+    @extend_schema(tags=["06. Post"])
     @action(detail=True, methods=["get"], url_path="feed")
     def feed(self, request: Request, *args, **kwargs):
         user: User = self.get_object()
@@ -188,48 +184,56 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 @extend_schema(tags=["04. Profile"])
-class ProfileViewSet(viewsets.ModelViewSet):
+class ProfileView(APIView):
     permission_classes = [IsAdminOrReadOnly]
     queryset = Profile.objects.all()
     serializer_class = UserProfileSerializer
-    filterset_class = ProfileFilter
 
-    def list(self, request: Request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
+    def get(self, request, user_pk):
+        user: User = User.objects.get(id=user_pk)
+        profile: Profile = get_object_or_404(Profile, user=user)
+
+        serializer = self.serializer_class(profile)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, user_pk):
+        user: User = User.objects.get(id=user_pk)
+        serializer = self.serializer_class(data=request.data, context={"user": user})
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+    def put(self, request, user_pk):
+        user: User = get_object_or_404(User, pk=user_pk)
+        instance: Profile = user.profile
+        serializer = self.serializer_class(instance, data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, user_pk):
+        user: User = User.objects.get(id=user_pk)
+        profile: Profile = user.profile
+
+        profile.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @extend_schema(tags=["05. Follow"])
-class FollowViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAdminOrReadOnly]
-    queryset = Follow.objects.all()
+class FollowListView(APIView):
     serializer_class = FollowSerializer
+    filter_backends = DjangoFilterBackend
     filterset_class = FollowFilter
 
-    def list(self, request: Request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
+    def get(self, request):
+        follow: Follow = Follow.objects.all()
+        queryset = self.filter_backends().filter_queryset(request, follow, self)
+
+        serializer = self.serializer_class(queryset, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-    def create(self, request: Request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            validated_data = serializer.validated_data
-            qs = Follow.objects.filter(**validated_data)
-            if qs.exists():
-                qs.delete()
-                return Response(
-                    data={"message": "Unfollowed successfully"},
-                    status=status.HTTP_204_NO_CONTENT,
-                )
-
-            serializer.save()
-            return Response(
-                data={"message": "Followed successfully"},
-                status=status.HTTP_201_CREATED,
-            )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @extend_schema(tags=["01. My"])
@@ -263,7 +267,7 @@ class OtherUserInfoView(APIView):
 
 
 @extend_schema(tags=["01. My"])
-class ProfileView(APIView):
+class MyProfileView(APIView):
     serializer_class = ProfileSerializer
 
     def get(self, request):
@@ -307,7 +311,7 @@ class OtherProfileView(APIView):
 
 
 @extend_schema(tags=["01. My"])
-class FollowView(APIView):
+class MyFollowView(APIView):
     def post(self, request, user_id):
         user: User = request.user
 
@@ -329,7 +333,7 @@ class FollowView(APIView):
 
 
 @extend_schema(tags=["01. My"])
-class FollowingView(APIView):
+class MyFollowingView(APIView):
     serializer_class = UserSerializer
     filter_backends = DjangoFilterBackend
     filterset_class = UserFilter
@@ -344,7 +348,7 @@ class FollowingView(APIView):
 
 
 @extend_schema(tags=["01. My"])
-class FollowerView(APIView):
+class MyFollowerView(APIView):
     serializer_class = UserSerializer
     filter_backends = DjangoFilterBackend
     filterset_class = UserFilter
@@ -359,7 +363,7 @@ class FollowerView(APIView):
 
 
 @extend_schema(tags=["01. My"])
-class FeedView(APIView):
+class MyFeedView(APIView):
     serializer_class = PostSerializer
     filter_backends = DjangoFilterBackend
     filterset_class = PostFilter
