6d805c3da12fe4b05daca464b0e5c8244d27554f
25th-Night <brown_eyed87@naver.com>
2023-09-19 19:07:38 +0900

    config/settings/base: DEFAULT_PARSER_CLASSES 순서 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -161,8 +161,8 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PARSER_CLASSES": [
         "rest_framework.parsers.JSONParser",
-        "rest_framework.parsers.FormParser",
         "rest_framework.parsers.MultiPartParser",
+        "rest_framework.parsers.FormParser",
     ],
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
