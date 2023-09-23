e9e44789e3e636d084b3305135f187d866daa538
25th-Night <brown_eyed87@naver.com>
2023-09-23 22:54:00 +0900

    infra/NCP/modules/vpc: vpc 모듈 생성

---
+++ infra/NCP/modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "ncp_access_key" {
+  type = string
+}
+
+variable "ncp_secret_key" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "ipv4_cidr_block" {
+  type = string
+}
